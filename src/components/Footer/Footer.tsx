357ab7bc10dde3798fbbc446c2614782b3587da0
Demetrius Sullivan <dssullivan1@gmail.com>
2024-12-10 21:24:56 -0500

    we finished creating our footer section which includes logo, about section, copyright and footer menu, styled the footer for all mobile sizes dec 10 2024 9:24pm

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import Logo from '../../assets/dsullivanrealty-logo.jpg';
+
 
 const Footer = () => {
   return (
   <>
       <div className={styles['FooterBar']}>
-          <div className={styles['topFooterWrap']}>
-              <div className={styles['footerAbout']}>
-                  <p>Logo</p>
-                
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Duis mollis et sem sed sollicitudin. Donec non odio neque. 
-                    Aliquam hendrerit sollicitudin purus, quis rutrum mi accumsan nec.
-                  </p>             
+          <div class={styles['footerAboutWrap']}>
+            <div className="footerLogo"><img src={Logo} alt="" /></div>
+              <div className="footerAbout">
+                <p>Lorem ipsum dolor sit amet, consectetur 
+                  adipiscing elit. Duis mollis et sem sed 
+                  sollicitudin. Donec non odio neque. 
+                  Aliquam hendrerit sollicitudin purus, 
+                  quis rutrum mi accumsan nec.
+                </p>
+                <p>Copyright - D.Sullivan Realty</p>
               </div>
           </div>
 
-          <div className={styles['bottomFooter']}>
-              <div className={styles['copyright']}>D.Sullivan Realty - All rights Reserved</div>
-              <div className={styles['socials']}>Social Media Links</div>
+          <div class={styles['footerNavWrap']}>
+                <ul>
+                  <li>Home</li>
+                  <li>About</li>
+                  <li>Properties</li>
+                  <li>Contact</li>
+                </ul>
           </div>
         
       </div>
