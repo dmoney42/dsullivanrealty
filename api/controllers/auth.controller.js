fd2e547be412bfb5e7866693273a93bf77871af6
Demetrius Sullivan <dssullivan1@gmail.com>
2024-12-18 12:59:06 -0500

    we created our auth controller to handle authentication, and created our error handling in index.js

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+
+export const signup = async (request, response, next) => {
+    //console.log("The signup request from the client is: " + JSON.stringify(request.body));
+
+    const {username, email, password} = request.body;
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const newUser = new User({username, email, password: hashedPassword});
+
+    try {
+        await newUser.save();
+        response.status(201).json("User created successfully!")
+    } catch (error) {
+       next(error);
+    }
+};
--- api/index.js
+++ api/index.js
@@ -2,7 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-import userRouter from "./routes/user.routes.js";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 const encodedUsername = encodeURIComponent(process.env.MONGODB_USERNAME);
 const encodedPassword = encodeURIComponent(process.env.MONGODB_PASSWORD);
@@ -18,8 +19,22 @@ mongoose.connect(`mongodb+srv://${encodedUsername}:${encodedPassword}@${host}/?r
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000");
 })
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((error, request, response, next)=>{
+    const statusCode = error.statusCode || 500;
+    const message = error.message || "Internal Server Erorr";
+
+    return response.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
