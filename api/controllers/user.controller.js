f4875fa80275f70b5e06c93aba9b4f168ae0ec56
Demetrius Sullivan <dssullivan1@gmail.com>
2025-02-13 10:57:14 -0500

    we have an issue with updating our profile using our dummy login credentials

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,17 +1,34 @@
 import bcrypt from "bcrypt";
+import User from "../models/user.model.js";
 
 export const test = (request, response) =>{
     response.send("Test route being called from a controller section of the app");
 }
 
-export const updateUser = (request, response, next) => {
+export const updateUser = async (request, response, next) => {
     if(request.user.id !== request.params.id) return next(errorHandler(401, "You can only update your own account."));
 
     try {
         if(request.body.password){
             request.body.password = bcrypt.hashSync(request.body.password, 10);
         }
+
+        const updatedUser = await User.findByIdAndUpdate(
+            request.params.id,
+            {
+                $set: {
+                    username: request.body.username,
+                    email: request.body.email,
+                    password: request.body.password,
+                    avatar: request.body.avatar,
+                }
+            },
+            { new: true}
+        );
+
+        const { password, ...restOfArray} = updatedUser._doc;
+        response.status(200).json(restOfArray);
     } catch (error) {
-        
+        next(error);
     }
 }
