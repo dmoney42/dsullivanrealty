bfd2988ccc0f4577d8250b4030502e714d542b03
Demetrius Sullivan <dssullivan1@gmail.com>
2025-04-10 00:17:05 -0400

    Fix: backend index.js for deployment

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ mongoose.connect(`mongodb+srv://${encodedUsername}:${encodedPassword}@${host}/?r
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "http://dsullivanrealty-client.s3-website-us-west-2.amazonaws.com",
     credentials: true,  // ✅ Allows cookies to be sent
 }));
 
